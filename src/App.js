41fb6032c9f2b749ef99eea5f1c472c14ccb40a2
Jali Pirhonen <32997880+jpir13@users.noreply.github.com>
2018-11-17 18:04:19 +0200

    js: mouseovers to h1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const label = full => {
+  if (full === null) {
+    return 'WWULTD?';
+  }
+    return 'what would you like to do?';
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { full: null };
+  }
+
+  mouseOut() {
+    console.log("mouse out");
+    this.setState({ full: null });
+  }
+
+  mouseOver() {
+    console.log("mouse hovered");
+    this.setState({ full: true });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>
-            WWULTD
+          <h1 onMouseOut={() => this.mouseOut()} onMouseOver={() => this.mouseOver()}>
+            {label(this.state.full)}
           </h1>
-          what would you like to do<b>?</b>
         </header>
         test
       </div>
