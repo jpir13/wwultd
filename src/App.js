8cefe41b41a897357689839a10cc442398914a65
Jali Pirhonen <32997880+jpir13@users.noreply.github.com>
2018-11-18 11:24:39 +0200

    changed how to load weather js, readme updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import Weather from './services/Weather';
+import Form from './services/Form';
 
-// displaying WWULTD, if full state null
+// displaying short name, if full state null
 const label = full => {
   if (full === null) {
     return 'WWULTD';
@@ -13,10 +15,19 @@ const thingsToDo = ['go golfing', 'sunbathing', 'see a theatre', 'go hiking', 's
 const listThings = thingsToDo.map((thing) => 
   <li><a href="#">{thing}</a></li>
   );
+const api_key = '083d0cdb1c7e13150cf1151ea50242bb';
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { full: null };
+    this.state = {  full: null,
+                    country: undefined,
+                    city: undefined,
+                    temperature: undefined,
+                    description: undefined,
+                    humidity: undefined,
+                    error: undefined
+    };
   }
 // simple mouseovers & out's for setting full either true or false
   mouseOver() {
@@ -27,6 +38,32 @@ class App extends Component {
   mouseOut() {
     console.log("mouse out");
     this.setState({ full: null });
+  }
+
+  state = {
+
+  }
+
+  getWeather = async (e) => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`);
+    const response = await api_call.json();
+    console.log(response);
+    if (city && country) {
+      this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        error: ""
+      })
+    } else {
+      this.setState({
+        error: "No search values.."
+      })
+    }
   }
 
   render() {
@@ -42,6 +79,15 @@ class App extends Component {
             Are you planning a holiday? Or perhaps you need a getaway from the dark Finnish fall?<br /> What ever the case is, this <i>React.js</i> site will provide an answer for you. Try the example <code>things to do</code> below.<br /> It will suggest suitable location for action in mind. 
             <ul>{listThings}</ul>
           </p>
+                  <Form loadWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
         </article>
       </div>
     );
