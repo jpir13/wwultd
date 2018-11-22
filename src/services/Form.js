3439fbdd9ee8bfb722db1529be532d59d02a9fd9
Jali Pirhonen <32997880+jpir13@users.noreply.github.com>
2018-11-22 21:01:21 +0200

    js & css: weather output formatting

--- src/services/Form.js
+++ src/services/Form.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
 
 class Form extends React.Component {
-    render(){
-        return(
-                <div>
-                <form onSubmit = {this.props.getWeather}>
-                    <input type="text" name="city" placeholder="City"/> &nbsp;
-                    <input type="text" name="country" placeholder="Country"/> <br></br><br></br>
-                    <button>Get Weather</button>
-                </form><br />
-                <button onClick={this.props.getWeatherSuggest}>Suggest me a location</button>
-                </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.getWeather}>
+          <input type="text" name="city" placeholder="City" /> &nbsp;
+          <input type="text" name="country" placeholder="Country" /> <br />{" "}
+          <br />
+          <button>Get Weather</button>
+        </form>
+        <br />
+        <button onClick={this.props.getWeatherSuggest}>
+          Suggest me a location
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Form;
--- src/services/Weather.js
+++ src/services/Weather.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React from "react";
 
 class Weather extends React.Component {
-
-    render() {
-
-        return (
-            <div className="weather-info">
-                {
-                    this.props.country && this.props.city && <p className="weather-field">Location:
-                        <span className="weather-value">  {this.props.city}, {this.props.country}</span>
-                    </p>
-                }
-
-                {
-                    this.props.temperature && <p className="weather-field">Temperature:
-                        <span className="weather-value">  {this.props.temperature}°</span>
-                    </p>
-                }
-
-                {
-                    this.props.description && <p className="weather-field">Conditions:
-                        <span className="weather-value">  {this.props.description}</span>
-                    </p>
-                }
-
-                {
-                    this.props.error && <p className="weather-error">{this.props.error}</p>
-                }
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="weather-info">
+        {this.props.country && this.props.city && (
+          <span className="weather-field">
+            Location:
+            <span className="weather-value">
+              {" "}
+              {this.props.city}, {this.props.country}
+            </span>
+          </span>
+        )}{" "}
+        &nbsp;
+        {this.props.temperature && (
+          <span className="weather-field">
+            Temperature:
+            <span className="weather-value"> {this.props.temperature}°</span>
+          </span>
+        )}{" "}
+        &nbsp;
+        {this.props.description && (
+          <span className="weather-field">
+            Conditions:
+            <span className="weather-value"> {this.props.description}</span>
+          </span>
+        )}
+        {this.props.error && (
+          <span className="weather-error">{this.props.error}</span>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Weather;
