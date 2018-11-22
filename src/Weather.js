4c1a2a1e4356f92c7e1f7b806f4ccd369c288590
Jali Pirhonen <32997880+jpir13@users.noreply.github.com>
2018-11-23 00:08:21 +0200

    Delete Weather.js

--- src/Weather.js
+++
@@ -1,6 +0,0 @@
-getWeather = async () => {
-    const api_key = '083d0cdb1c7e13150cf1151ea50242bb';
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${api_key}`);
-    const response = await api_call.json();
-    console.log(response);
-}
